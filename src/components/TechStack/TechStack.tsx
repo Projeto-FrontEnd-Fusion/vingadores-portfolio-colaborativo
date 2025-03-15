6cb1710c9da58df066329cfb13e515a25b19436e
Maria de Fátima <mnunesalves334@gmail.com>
2025-03-15 15:14:21 -0300

    feat: ajustando breakpoints para deixar o site responsivo

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -6,9 +6,9 @@ const techs = [
     "Node.js", "Next.js", "PostgreSQL", "Prisma ORM", "MongoDB"
 ];
 
-const TechStack: React.FC = () => {
+const TechStack = () => {
     return (
-        <section className="py-16 text-center bg-[#0a0a23] text-white">
+        <section className="py-16 text-center text-white">
             <h2 className="text-3xl font-bold">Impulsionados por tecnologia</h2>
             <p className="text-lg text-gray-400 mt-2">
                 As linguagens de programação que fazem nosso projeto ganhar vida
@@ -19,16 +19,15 @@ const TechStack: React.FC = () => {
                     modules={[Autoplay]}
                     autoplay={{ delay: 2000, disableOnInteraction: false }}
                     loop={true}
-                    slidesPerView={"auto"}
-                    spaceBetween={30}
+                    spaceBetween={10}
                     centeredSlides={false}
                     breakpoints={{
-                        320: { slidesPerView: 2, spaceBetween: 15 },
-                        480: { slidesPerView: 3, spaceBetween: 20 },
-                        768: { slidesPerView: 4, spaceBetween: 25 },
-                        1024: { slidesPerView: 5, spaceBetween: 30 },
-                        1440: { slidesPerView: 6, spaceBetween: 35 },
-                        1920: { slidesPerView: 7, spaceBetween: 40 },
+                        320: { slidesPerView: 2, spaceBetween: 10 },
+                        480: { slidesPerView: 3, spaceBetween: 15 },
+                        768: { slidesPerView: 4, spaceBetween: 20 },
+                        1024: { slidesPerView: 6, spaceBetween: 20 },
+                        1440: { slidesPerView: 6, spaceBetween: 20 },
+                        1920: { slidesPerView: 12, spaceBetween: 20 },
                     }}
                     aria-label="Carrossel de tecnologias utilizadas no projeto"
                 >
@@ -38,7 +37,6 @@ const TechStack: React.FC = () => {
                                 <div className="absolute inset-0 rounded-full bg-[radial-gradient(circle,_rgba(100,109,240,0.5)_8%,_rgba(30,30,80,0.3)_60%,_rgba(10,9,32,0)_70%)] shadow-[0_0_20px_8px_rgba(100,109,240,0.5)])]"></div>
                                 <p className="relative text-lg font-semibold text-white">{tech}</p>
                             </div>
-
                         </SwiperSlide>
                     ))}
                 </Swiper>
